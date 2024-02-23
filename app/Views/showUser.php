ab19421b86f2119ba10bc7f66a5fb76b55982555
Ranie <143940800+ranyesantos@users.noreply.github.com>
2024-02-23 20:08:51 -0300

    new functions added: registes and edit

--- app/Views/showUser.php
+++ app/Views/showUser.php
@@ -6,17 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>users list</h1>
+    <h1>Detalhes</h1>
     <ul>
         <li>
             Nome: <?= $user['nome']?> <br>
             CPF: <?= $user['cpf']?> <br>
-            Data de Nascimento: <?=(new DateTime($user['nasc']))->format('d/m/Y')?> <br>
+            Data de Nascimento: <?= $user['nasc']?> <br>
             <button>
                 <a href="index.php?action=deleteUser&id=<?= $user['id']?>">
                     apagar
                 </a>
             </button>
+            <form action="app/views/edit.php" method="POST">
+                <input type="hidden" name="id" value="<?= $user['id'] ?>">
+                <input type="hidden" name="nome" value="<?= $user['nome'] ?>">
+                <input type="hidden" name="cpf" value="<?= $user['cpf'] ?>">
+                <input type="hidden" name="nasc" value="<?= $user['nasc'] ?>">
+                <button type="submit">Editar</button>
+            </form>
         </li>
     </ul>
 </body>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     
     //instanciando o model e concedendo acesso ao banco de dados
     $model = new UserModel($db);
-    
+
     //instanciando o controller e concedendo acesso ao model
     $controller = new UserController($model);
 
@@ -16,6 +16,13 @@
     
     //recebe o id de um elemento para que seja realizado alguma ação naquele elemento em específico
     $id = isset($_GET['id']) ? ($_GET['id']) : null;
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nome"]) && isset($_POST["cpf"]) && isset($_POST["nasc"])) {
+        $id = $_POST["id"];
+        $nome = $_POST["nome"];
+        $cpf = $_POST["cpf"];
+        $nasc = $_POST["nasc"];
+    }
 
     switch ($action) {
 
@@ -31,11 +38,18 @@
             $controller->deleteUser($id);
             break;
 
+        case 'addUser':
+            $controller->addUser($nome, $cpf, $nasc);
+            break;
+
+        case 'editUser':
+            $controller->editUser($id, $nome, $cpf, $nasc);
+            break;
+
         default:
             echo "ação inválida";
             break;
 
     }
-
     
 ?> 
